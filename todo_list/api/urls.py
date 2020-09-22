512f3ce9246e5eb960123d09de8034c70fd16b89
ziad nabil <ziadmoh2525@gmail.com>
2020-09-22 23:17:54 +0200

    ''

--- todo_list/api/urls.py
+++ todo_list/api/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.apiOverview, name="api-overview")
+    path('', views.apiOverview, name="api-overview"),
+    path('task-list/', views.taskList, name="task-list"),
+    path('task-detail/<str:pk>/', views.taskDetail, name="task-detail"),
+
 ]
--- todo_list/api/views.py
+++ todo_list/api/views.py
@@ -3,18 +3,34 @@ from django.http import JsonResponse
 
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .serializers import TaskSerializer
 
+from .models import Task
 # Create your views here.
 
 @api_view(['GET'])
-
 def apiOverview(request):
 
     api_urls = {
-        'List' : '/task-list/',
-        'Detail View' : '/task-detail/<str:pk>',
-        'Create' : '/task-create/',
-        'Update' : '/task-update/<str:pk>',
-        'Delete' : '/task-delete/<str:pk>'
+        'List':'/task-list/',
+        'Detail View':'/task-detail/<str:pk>/',
+        'Create':'/task-create/',
+        'Update':'/task-update/<str:pk>/',
+        'Delete':'/task-delete/<str:pk>/',
     }
+
     return Response(api_urls)
+
+@api_view(['GET'])
+def taskList(request):
+    tasks = Task.objects.all()
+    serializer = TaskSerializer(tasks, many = True)
+
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def taskDetail(request, pk):
+    tasks = Task.objects.get(id=pk)
+    serializer = TaskSerializer(tasks, many = False)
+
+    return Response(serializer.data)
