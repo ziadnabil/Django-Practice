b71388e2c2fe9cde8ba4fd97b160d9a3a8a918ed
ziadnabil <ziadmoh2525@gmail.com>
2020-10-02 14:22:53 +0200

    job model

--- first_project/board/src/job/models.py
+++ first_project/board/src/job/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+JOB_CHOICES = (
+    ('Full Time', 'Full Time'),
+    ('Part Time', 'Part Time'),
+)
+
+class Job(models.Model):
+    title         = models.CharField(max_length=100)
+    # location
+    job_type      = models.CharField(choices = JOB_CHOICES, max_length=20)
+    description   = models.TextField(max_length=2000)
+    published_At  = models.DateTimeField(auto_now=True)
+    positions_num = models.IntegerField(default=1)
+    salary        = models.IntegerField(default=0)
+    experience    = models.IntegerField(default=1)
+
+    def __str__(self):
+        return self.title
